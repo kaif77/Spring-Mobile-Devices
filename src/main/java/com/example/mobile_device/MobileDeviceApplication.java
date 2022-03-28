9466dded9e33be3304d7f1cd7291f7fd14c57c21
kaif77 <kaifmuhammed2@gmail.com>
2022-03-28 15:58:54 +0530

    Initial Commit

---
+++ src/main/java/com/example/mobile_device/MobileDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobile_device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileDeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mobile_device/MobileDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobile_device;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
